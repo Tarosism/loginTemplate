c12c470c8e48b689cce55852a66e78c74869e224
Tarosism <yar0606@naver.com>
2021-11-29 19:58:03 +0900

    template start

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`port is ${PORT}`);
+});
